922fb0eb48229103864211d09e06e91d93987bbc
LucasFlaquer <lucas.flaquer@gmail.com>
2020-08-07 19:26:49 -0300

    app wep concluido

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,30 +2,52 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+  name: string;
+  avatar: string;
+  bio:string;
+  cost:number;
+  id:number;
+  subject:string;
+  whatsapp:string;
+}
+
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/22601978?s=460&u=4ef4328d281f34dc2212e0c871d6917dcfde86d9&v=4" alt="LucasFlaquer"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div className="">
-          <strong>Lucas Flaquer</strong>
-          <span>Física</span>
+          <strong>{teacher.subject}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, atque.
-        <br/><br/>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur expedita eveniet molestias tempora quibusdam voluptatibus dicta ipsa qui hic aperiam.
+        {teacher.bio}
       </p>
       <footer>
         <p>
-          preço/hora <strong>R$ 80,00</strong>
+          preço/hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a onClick={createNewConnection} href={`http://wa.me/${teacher.whatsapp}`} target="_blank">
           <img src={whatsappIcon} alt="whatsapp"/>
           entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
